cc86affccf3514cdacb7263bb8c19ade8eadf4d9
[Grant Willis] <[thegwillis.gw@gmail.com]>
2024-06-24 23:28:53 -0700

    Wrote createThread function and createThread route. Added REST routes for testing purposes.

--- routes/thread.js
+++ routes/thread.js
@@ -4,5 +4,5 @@ const router = express.Router();
 const threadController = require('../controllers/thread');
 
 router.get('/', threadController.getAll);
-
+router.post('/', threadController.createThread);
 module.exports = router;
