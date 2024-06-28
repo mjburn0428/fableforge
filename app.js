fd2b2ceacd1fb7c0848787351499de769b97a24d
JBuzz826 <59944676+JBuzz826@users.noreply.github.com>
2024-06-27 21:32:41 -0600

    updated *crossing fingers*

--- app.js
+++ app.js
@@ -6,21 +6,30 @@ const app = express();
 
 const port = process.env.port || 8080;
 
-app
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
+// Apply CORS middleware
+app.use(cors());
 
-  app.use(cors());
-  
+// Body parser middleware
+app.use(bodyParser.json());
+
+// Custom middleware to set headers (Optional if using CORS middleware)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// Route middleware
+app.use('/', require('./routes'));
+
+// Initialize DB and start server
 mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
-    app.listen(port);
-    console.log(`Connected to DB and listening on Port ${port}`);
+    app.listen(port, () => {
+      console.log(`Connected to DB and listening on Port ${port}`);
+    });
   }
 });
--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: 'Description'
   },
   host: 'fableforge.onrender.com',
-  schemes: ['http']
+  schemes: ['https']
 };
 
 const outputFile = './swagger-output.json';
